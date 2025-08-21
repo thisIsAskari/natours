f280a7f0014087e5485f04ce819aa1589bedca27
thisisaskari <muhammadaskari000@gmail.com>
2025-08-21 08:11:54 +0500

    187-Including a Map with Mapbox Part 2

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -2,3 +2,46 @@
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
 console.log(locations);
+
+// mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+// const map = new mapboxgl.Map({
+//   style: 'mapbox://styles/mapbox/streets-v12',
+// scrollZoom: false,
+//   container: 'map', // container ID
+//   center: [-118.113491, 34.111745], // starting position [lng, lat]. Note that lat must be set between -90 and 9
+//   zoom: 10, // starting zoom
+//   interactive: false
+// });
+
+// const bounds = new mapboxgl.LngLatBounds();
+// locations.forEach((loc) => {
+//   // create marker
+//   const el = document.createElement('div');
+//   el.className = 'marker';
+//   // add marker
+//   new mapboxgl.Marker({
+//     element: el,
+//     anchor: 'bottom',
+//   })
+//     .setLngLat(loc.coordinates)
+//     .addTo(map);
+//   // add popup
+//   new mapboxgl.Popup({
+//     offset: 30,
+//   })
+//     .setLngLat(loc.coordinates)
+//     .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+//     .addTo(map);
+
+//   // extends map bounds to include current location
+//   bounds.extend(loc.coordinates);
+// });
+
+// map.fitBounds(bounds, {
+//   padding: {
+//     top: 200,
+//     bottom: 150,
+//     left: 100,
+//     right: 100,
+//   },
+// });
