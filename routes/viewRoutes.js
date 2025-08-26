cee4c2e4ec3ca95ba7232fac2505bdca475de47b
thisisaskari <muhammadaskari000@gmail.com>
2025-08-26 07:48:35 +0500

    194-Building the User Account Page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -12,12 +12,11 @@ const router = express.Router();
 //     user: 'Askari',
 //   });
 // });
+router.get('/me', authController.protect, viewsController.getAccount);
 
 router.use(authController.isLoggedIn);
-
 router.get('/', viewsController.getOverview);
 router.get('/tour', viewsController.getTour);
-
 router.get('/tour/:slug', viewsController.getTour);
 router.get('/login', viewsController.getLoginForm);
 
