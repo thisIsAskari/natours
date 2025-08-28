d997d46a19bfef6198847bf7dc19fabba9617172
thisisaskari <muhammadaskari000@gmail.com>
2025-08-28 07:17:06 +0500

    199-Image Upload Using Multer Package, Multer used to post form with enctype="multipart/form-data"

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
+const upload = multer({
+  dest: 'public/img/users',
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+      cb(null, true);
+    } else {
+      cb(new AppError('Not an image! Please upload only images', 400), false);
+    }
+  },
+});
 // auth
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
@@ -17,7 +31,7 @@ router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updateMe);
+router.patch('/updateMe', upload.single('photo'), userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
@@ -25,7 +39,7 @@ router.use(authController.restrictTo('admin'));
 //users
 router
   .route('/')
-  .post(userController.createUser)
+  .post(upload.single('photo'), userController.createUser)
   .get(userController.getAllUsers);
 router
   .route('/:id')
