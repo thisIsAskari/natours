42efaec4175646bd4ebddcd06f4324f1db475785
thisisaskari <muhammadaskari000@gmail.com>
2025-08-31 14:04:27 +0500

    211-Integrating Stripe into the BackEnd

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 
 const app = express();
 
@@ -104,6 +105,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 // 404 Route
 app.all('*', (req, res, next) => {
---
+++ routes/bookingRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bookingController = require('../controllers/reviewController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+router.get(
+  '/checkout-session/:tourId',
+  authController.protect,
+  bookingController.getCheckoutSession,
+);
+
+module.exports = router;
