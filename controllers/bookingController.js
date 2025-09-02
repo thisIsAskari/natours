1ab87cd0b76ac1a93c7bed456287afb653e92a52
thisisaskari <muhammadaskari000@gmail.com>
2025-09-02 21:16:38 +0500

    216-Finishing the Bookings API

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -47,3 +47,12 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   await Booking.create({ tour, user, price });
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking, {
+  path: 'tour',
+  select: 'name',
+});
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
